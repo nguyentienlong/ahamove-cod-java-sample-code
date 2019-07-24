6b8d9adef6857f9a4d76ffe15112b16e1775ab7c
longkyanh <long@ahamove.com>
2019-07-24 17:55:36 +0700

    Add top up example

--- src/main/java/com/ahamove/cod/App.java
+++ src/main/java/com/ahamove/cod/App.java
@@ -28,29 +28,51 @@ public class App
         try {
             JSONArray orders = retrieveCodInfo(supplierId);
             // try to get first order then pay cod
-            System.out.println("Pay cod for " + orders.getJSONObject(0).getString("id"));
+            System.out.println("## Pay cod for " + orders.getJSONObject(0).getString("id"));
             // pay Cod
             payCod(supplierId, orders.getJSONObject(0));
+
+            // top up for supplier type
+            System.out.println("## Top up for supplier " + supplierId + " amount " + 10000);
+            topUp(supplierId, "supplier", 10000);
+
+            // top up for user type
+            System.out.println("## Top up for user " + supplierId + " amount " + 20000);
+            topUp(supplierId, "user", 20000);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
     /**
-     * payCod
-     * @param supplierId
-     * @param order
+     * topUp
+     * @param String phoneNumber
+     * @param String userType
+     * @param int amount
      * @throws Exception
      */
-    public static void payCod(String supplierId, JSONObject order) throws Exception
+    private static void topUp(String phoneNumber, String userType, int amount) throws Exception
     {
         JSONObject payload = new JSONObject();
         String transactionId =  Long.toString(new Date().getTime());
         payload.put("transaction_id", transactionId);
-        payload.put("supplier_id", supplierId);
+        payload.put("phone_number", phoneNumber);
+        payload.put("user_type", userType);
         payload.put("partner_id", PARTNER_ID);
-        payload.put("order_id", order.getString("id"));
-        payload.put("amount", order.getInt("total_cod"));
+        payload.put("amount", amount);
+
+        String url = BASE_URL + VERSION + "/cod/top_up";
+        sendPostRequest(payload, url);
+    }
+
+    /**
+     * sendPostRequest
+     * @param JSONObject payload
+     * @param String url
+     * @throws Exception
+     */
+    private static void sendPostRequest(JSONObject payload, String url) throws Exception{
         System.out.print("payload");
         System.out.println(payload.toString());
 
@@ -64,7 +86,6 @@ public class App
         System.out.println("encryptPayload");
         System.out.println(encryptPayload);
 
-        String url = BASE_URL + VERSION + "/cod/pay";
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -91,16 +112,35 @@ public class App
         System.out.println(decryptedResult);
 
         con.disconnect();
+    }
 
+    /**
+     * payCod
+     * @param String supplierId
+     * @param JSONObject order
+     * @throws Exception
+     */
+    private static void payCod(String supplierId, JSONObject order) throws Exception
+    {
+        JSONObject payload = new JSONObject();
+        String transactionId =  Long.toString(new Date().getTime());
+        payload.put("transaction_id", transactionId);
+        payload.put("supplier_id", supplierId);
+        payload.put("partner_id", PARTNER_ID);
+        payload.put("order_id", order.getString("id"));
+        payload.put("amount", order.getInt("total_cod"));
+
+        String url = BASE_URL + VERSION + "/cod/pay";
+        sendPostRequest(payload, url);
     }
 
     /**
      * retrieveCodInfo
-     * @param supplierId
+     * @param String supplierId
      * @return
      * @throws Exception
      */
-    public static JSONArray retrieveCodInfo(String supplierId) throws Exception
+    private static JSONArray retrieveCodInfo(String supplierId) throws Exception
     {
         String url =
                 BASE_URL + VERSION + "/cod/retrieve_info?partner_id=" + PARTNER_ID + "&supplier_id=" + supplierId;
@@ -117,18 +157,16 @@ public class App
         String decryptedResult = decrypt(bufferedReader);
         con.disconnect();
 
-        JSONArray orders = new JSONArray(decryptedResult);
-
-        return orders;
+        return new JSONArray(decryptedResult);
     }
 
     /**
      * decrypt
-     * @param in
+     * @param BufferedReader in
      * @return
      * @throws Exception
      */
-    public static String decrypt(BufferedReader in) throws Exception
+    private static String decrypt(BufferedReader in) throws Exception
     {
         String inputLine;
         StringBuffer response = new StringBuffer();
